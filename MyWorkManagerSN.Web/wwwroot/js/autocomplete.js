71b7c4724cce3469cc00f32f196306574508c77a
samba914 <sambadiarra2000@gmail.com>
2022-03-19 14:08:40 +0100

    datatables changes

--- MyWorkManagerSN.Web/wwwroot/js/autocomplete.js
+++ MyWorkManagerSN.Web/wwwroot/js/autocomplete.js
@@ -1,4 +1,5 @@
-﻿function autocomplete(inp, seachType = null) {
+﻿
+function autocomplete(inp, seachType = null) {
     /*the autocomplete function takes two arguments,
     the text field element and an array of possible autocompleted values:*/
     var currentFocus;
---
+++ MyWorkManagerSN.Web/wwwroot/js/sortTable.js
@@ -0,0 +1,34 @@
+﻿const compare = (ids, asc) => (row1, row2) => {
+      const tdValue = (row, ids) => row.children[ids].textContent;
+      const tri = (v1, v2) => v1 !== '' && v2 !== '' && !isNaN(v1) && !isNaN(v2) ? v1 - v2 : v1.toString().localeCompare(v2);
+
+
+      var type= row1.children[ids].getAttribute("data-type");
+      if(type=="amount"){
+          var a = tdValue(asc ? row1 : row2, ids).split(" ")[0].replace(",", ".");;
+          var b = tdValue(asc ? row2 : row1, ids).split(" ")[0].replace(",", ".");;
+          return parseFloat(a) - parseFloat(b);
+      }else{
+          return tri(tdValue(asc ? row1 : row2, ids), tdValue(asc ? row2 : row1, ids));
+      }
+
+
+    };
+
+    const tbody = document.querySelector('tbody');
+    const thx = document.querySelectorAll('th');
+    const trxb = tbody.querySelectorAll('tr');
+    thx.forEach(th => th.addEventListener('click', () => {
+      $("#arrawDownSort").remove();
+      $("#arrawUpSort").remove();
+      if(!this.asc){
+          $(th).append("<i id='arrawDownSort' class='fa fa-arrow-down ml-1' aria-hidden='true'></i>");
+
+      }else{
+           $(th).append("<i id='arrawUpSort' class='fa fa-arrow-up ml-1' aria-hidden='true'></i>");
+      }
+
+
+      let classe = Array.from(trxb).sort(compare(Array.from(thx).indexOf(th), this.asc = !this.asc));
+      classe.forEach(tr => tbody.appendChild(tr));
+    }));
